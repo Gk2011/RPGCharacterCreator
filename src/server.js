c2dd6412d5e32fa5b27f881a230eb4aa0b4bfb4e
Gk2011 <Gk2011@gmail.com>
2021-05-19 17:58:20 -0400

    test

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ const viewChracterRoutes = require('./routes/viewCharacterRoutes');
 // Models
 const Character = require('./models/character');
 const { server_info } = require('./util/RedisClient');
-
+//comment
 const createApp = async () => {
     const app = express();
 
